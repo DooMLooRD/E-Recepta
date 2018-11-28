cb3c6bb6ed227931da66b4787bedcc39f814944f
210165 Dominiak Mateusz <210165@edu.p.lodz.pl>
2018-11-28 11:30:35 +0100

    added async methods

--- E-Recepta/MedicinesDatabase/MedicinesDB.cs
+++ E-Recepta/MedicinesDatabase/MedicinesDB.cs
@@ -10,91 +10,100 @@ namespace MedicinesDatabase
 {
     public class MedicinesDB
     {
-        private SqlConnection connection;
-        public MedicinesDB()
-        {
-            
-        }       
-
-        public bool Connect()
-        {
-            try
-            {
-                connection = new SqlConnection(@"Data source=LOCALHOST\SQLEXPRESS;
+        private string connectionString = @"Data source=iolab.database.windows.net,1433;
                                                              database=MedicinesDB;
-                                                             User id=sa;
-                                                             Password=Erecepta;");
-                connection.Open();
-            }
+                                                             User id=dbuser;
+                                                             Password=Erecepta!;";
+          
 
-            catch (SqlException e)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public bool CloseConnection()
+        public async Task<IEnumerable<Medicine>> SearchMedicine(string name)
         {
-            try
-            {
-                connection.Close();
-            }
-            catch (SqlException e)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public IEnumerable<Medicine> SearchMedicine(string name)
-        {
-            SqlCommand command = connection.CreateCommand();
-            string commandText = "SELECT * FROM medicines WHERE name LIKE '%" + name + "%'";
-            command.CommandText = commandText;
-            SqlDataReader reader = command.ExecuteReader();
-
             List<Medicine> medicines = new List<Medicine>();
 
-            while (reader.Read())
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                medicines.Add(new Medicine(reader["id"].ToString(), reader["name"].ToString(),
-                                           reader["manufacturer"].ToString(), reader["refund_rate"].ToString()));
+                try
+                {
+                    conn.Open();
+                    SqlCommand command = conn.CreateCommand();
+                    string commandText = "SELECT * FROM medicines WHERE name LIKE '%" + name + "%'";
+                    command.CommandText = commandText;
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (reader.Read())
+                    {
+                        medicines.Add(new Medicine(reader["id"].ToString(), reader["name"].ToString(),
+                                                   reader["manufacturer"].ToString(), reader["refund_rate"].ToString()));
+                    }
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+                    medicines = null;
+                }
             }
-            reader.Close();
 
             return medicines;
         }
 
-        public bool IsMedicineInDB(string id)
+        public async Task<int> IsMedicineInDB(string id)    //returns 1 when id is in DB, 0 when it's not and -1 when exception occurs
         {
-            SqlCommand command = connection.CreateCommand();
-            string commandText = "SELECT * FROM medicines WHERE id = " + id;
-            command.CommandText = commandText;
-            SqlDataReader reader = command.ExecuteReader();
+            int response = -1;
 
-            bool hasRows = reader.HasRows;
-            reader.Close();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand command = conn.CreateCommand();
+                    string commandText = "SELECT * FROM medicines WHERE id = " + id;
+                    command.CommandText = commandText;
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
 
-            return hasRows;
+                    bool hasRows = reader.HasRows;
+
+                    if (hasRows)
+                        response = 1;    
+                    else
+                        response = 0;
+
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+                   
+                }
+
+            }
+
+            return response;
         }
 
-        public float GetRateOfRefund(String id)
+        public async Task<float> GetRateOfRefund(String id)   //returns -1 when exception occurs
         {
-            SqlCommand command = connection.CreateCommand();
-            string commandText = "SELECT * FROM medicines WHERE id = " + id;
-            command.CommandText = commandText;
-            SqlDataReader reader = command.ExecuteReader();
-
             float refundRate = -1;
 
-            while (reader.Read())
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                refundRate = float.Parse(reader["refund_rate"].ToString());
+                try
+                {
+                    conn.Open();
+                    SqlCommand command = conn.CreateCommand();
+                    string commandText = "SELECT refund_rate FROM medicines WHERE id = " + id;
+                    command.CommandText = commandText;
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (reader.Read())
+                    {
+                        refundRate = float.Parse(reader["refund_rate"].ToString());
+                    }
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+
+                }
             }
-            reader.Close();
 
             return refundRate;
         }
