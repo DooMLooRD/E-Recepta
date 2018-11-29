eb06951bf5c0468a2474c6ae1805b8f907faf999
Jan Kisielewicz <janekk97@users.noreply.github.com>
2018-11-29 14:52:16 +0100

    classes for authorisation added

---
+++ E-Recepta/Authorisation/HashGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorisation
+{
+    class HashGenerator
+    {
+        static public string getHashFromPassword (string password)
+        {
+            byte[] tmpSource;
+            byte[] tmpHash;
+            string hash;
+
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(password);
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+
+            hash = ASCIIEncoding.ASCII.GetString(tmpHash);
+
+            return hash;
+        }
+    }
+}
---
+++ E-Recepta/Authorisation/UserDataBaseData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDatabaseAPI.Service;
+
+namespace Authorisation
+{
+    class UserDatabaseData
+    {
+        LoginService loginService;
+
+        public string getPasswordHash(string login, string role)
+        {
+            return loginService.GetPasswordHash(login, role).Result;
+        }
+
+        public void saveLoginAttempt(string username, DateTime loginTime, bool isSuccesful)
+        {
+            LoginAttemptDTO laDTO = new LoginAttemptDTO { Username = username, LoginTime = loginTime, IsSuccessful = isSuccesful };
+            bool x = loginService.AddLoginAttempt(laDTO).Result; //po co mnie ten bool
+            return;
+        }
+    }
+}
